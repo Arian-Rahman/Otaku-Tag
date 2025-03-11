156d659a82858a86486e0f6443c402821257f3f2
Arian <arianrahman431@gmail.com>
2025-03-12 02:04:13 +0600

    ...

--- gradio_client_test.py
+++
@@ -1,8 +0,0 @@
-from gradio_client import Client
-
-client = Client("soothsayer1221/Otaku-Tag")
-result = client.predict(
-		description="Naruto is a strong fighter!!",
-		api_name="/predict"
-)
-print(result)
